633445bca69db9c9bcbe17052972c6922b4916a7
Zeta <francodavidp@gmail.com>
2023-04-07 15:10:22 -0300

    Version pre-entrega

---
+++ js/formulario.js
@@ -0,0 +1,9 @@
+function Login(param1) {
+
+let correo = document.getElementById('form-email').value;
+let rango = document.getElementById('form-rank').value;
+let referente = document.getElementById('form-ref').value;
+let mensaje = document.getElementById('form-msg').value;
+let formulario = "Hola! Soy " + rango + "!" + " Me interesa conocer el Estudio! Supe ustedes gracias a " + referente + ". " + mensaje + "Mi dirección de mail para contacto es: " + correo +  ".Muchas gracias!"
+console.log(formulario);
+}
