f6b837575fdb84b36cc17fc206f70a7739807063
Zeta <francodavidp@gmail.com>
2023-04-16 23:50:31 -0300

    Presentacion final

--- js/formulario.js
+++ js/formulario.js
@@ -1,9 +1,7 @@
-function Login(param1) {
-
-let correo = document.getElementById('form-email').value;
-let rango = document.getElementById('form-rank').value;
-let referente = document.getElementById('form-ref').value;
-let mensaje = document.getElementById('form-msg').value;
-let formulario = "Hola! Soy " + rango + "!" + " Me interesa conocer el Estudio! Supe ustedes gracias a " + referente + ". " + mensaje + "Mi dirección de mail para contacto es: " + correo +  ". Muchas gracias!"
-console.log(formulario);
+function formulario(param1) {
+let nombre = document.getElementById('form-nombre').value;
+let correo = document.getElementById('form-mail').value;
+let mensaje = document.getElementById('form-mensaje').value;
+let cadena = nombre + " " + correo + " " + mensaje;
+console.log(cadena);
 }
